e6ebcc0ff1143cfab4507ac5de9a290ff11c03a7
Travis Luong <travis@travisluong.com>
2022-01-08 13:59:08 -0800

    spring

---
+++ spring/demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/")
+    public List<Map<String, Object>> hello() {
+        String sql = "SELECT * FROM \"Post\";";
+        List<Map<String, Object>> queryForList = jdbcTemplate.queryForList(sql);
+        // System.out.println(queryForList);
+        // for (int i = 0; i < queryForList.size(); i++) {
+        //     System.out.println(queryForList.get(i));
+        // }
+        return queryForList;
+    }
+}
