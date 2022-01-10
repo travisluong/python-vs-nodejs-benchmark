800ff51229ab38544199198aaf66485a5096082d
Travis Luong <travis@travisluong.com>
2022-01-09 18:30:57 -0800

    fastify

---
+++ fastify/server.js
@@ -0,0 +1,34 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+const { Pool, Client } = require('pg')
+const express = require('express')
+const app = express()
+const port = 3000
+
+const client = new Client({
+  user: 'benchmark_user',
+  host: 'localhost',
+  database: 'benchmark_db',
+  port: 5432,
+})
+client.connect()
+
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+    const resp = await client.query('SELECT * FROM \"Post\" LIMIT 100');
+    return resp.rows;
+//   return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
